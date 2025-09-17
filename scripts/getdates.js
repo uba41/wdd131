a88031cf606c56db4d599e8fb0d15cbde747a338
uba41 <ubagabriel41@gmail.com>
2025-09-17 03:09:47 -0700

    live

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+ document.getElementById('lastModified').textContent = 
+    "Last Modified: " + document.lastModified;
